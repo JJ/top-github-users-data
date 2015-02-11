456197ab12cc7bde6c28227aa6dd72a2e5f04aa9
JJ Merelo <jjmerelo@gmail.com>
2015-02-11 08:12:22 +0100

    Complete data per province

--- agg-csv.pl
+++ agg-csv.pl
@@ -11,16 +11,29 @@ use JSON;
 
 use File::Slurp::Tiny qw(read_file);
 
+my $provincias_content = read_file('poblacion-provincia-INE.csv');
+my @provincias = split("\n",$provincias_content);
+my @prov_names = split(",",$provincias[0]);
+my @prov_pop = split(",",$provincias[1]);
+
+my %new_names = ( "Alicante/Alacant" => "Alicante",
+		  "Araba/Álava" => "Álava",
+		  "Bizkaia" => "Bilbao",
+		  "Castellón/Castelló" => "Castellón",
+		  "Gipuzkoa" => "Donostia",
+		  "Girona" => "Gerona",
+		  "Palmas" => "Las Palmas",
+		  "Valencia/València" => "Valencia");
+
 my @columns =  qw( contributions stars followers );
-my @provincias = qw(Madrid Barcelona Valencia Alicante Sevilla Málaga Murcia 
-Cádiz Bilbao Coruña Baleares Asturias Tenerife Zaragoza 
-Pontevedra Granada Tarragona Córdoba Gerona);
 
-push @provincias, "Las Palmas"; 
-
-say "province;users;",join(";",@columns);
-for my $p ( @provincias ) {
-  my $file_contents = read_file("data/user-data-$p.json");
+say "province;population;users;",join(";",@columns);
+for my $p ( @prov_names ) {
+  my $population = shift @prov_pop;
+  my $name = $new_names{$p}?$new_names{$p}:$p;
+  next if $name eq "Guadalajara"; #Problems with sampling
+  my $file_contents = read_file("data/user-data-$name.json");
+  next if !$file_contents;
   my $p_data = decode_json( $file_contents);
   my $totals = {};
   for my $c (@columns) {
@@ -34,7 +47,7 @@ for my $p ( @provincias ) {
       ( $totals->{$column} += $u->{$column} ) if $u->{$column};
     }
   }
-  my @row = ( $p, $users );
+  my @row = ( $name, $population, $users );
   for my $column ( @columns ) {
     push @row, $totals->{$column};
   }
