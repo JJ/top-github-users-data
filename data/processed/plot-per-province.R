503c851cd60adfd7d610e8ae2e50efa6495fc8bf
JJ Merelo <jjmerelo@gmail.com>
2015-04-09 07:56:42 +0200

    Añadiendo más gráficos relativos

--- data/processed/plot-per-province.R
+++ data/processed/plot-per-province.R
@@ -1,6 +1,8 @@
 data.province <- read.csv('data-per-province.csv',sep=';')
 data.province$usersrel <- data.province$users / data.province$population
 data.province$contribrel <- data.province$contributions / data.province$population
+data.province$followersrel <- data.province$followers / data.province$population
+data.province$starsrel <- data.province$stars / data.province$population
 library(ggplot2)
 ggplot(data.province, aes(x=reorder(province,population),y=users))+geom_bar(stat='identity')+coord_flip()
 ggsave("users-by-province.png")
@@ -8,3 +10,7 @@ ggplot(data.province, aes(x=reorder(province,usersrel),y=usersrel))+geom_bar(sta
 ggsave("users-rel-by-province.png")
 ggplot(data.province, aes(x=reorder(province,contribrel),y=contribrel))+geom_bar(stat='identity')+coord_flip()
 ggsave("contrib-rel-by-province.png")
+ggplot(data.province, aes(x=reorder(province,followersrel),y=followersrel))+geom_bar(stat='identity')+coord_flip()
+ggsave("followers-rel-by-province.png")
+ggplot(data.province, aes(x=reorder(province,starsrel),y=starsrel))+geom_bar(stat='identity')+coord_flip()
+ggsave("stars-rel-by-province.png")
